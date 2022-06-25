ed77023484d4fa5237a834bf1c75cec1c14d45a0
thangved <thangved02@gmail.com>
2022-06-25 08:09:30 +0700

    Auto get cookie, cache cookie

--- utils/findByKey.js
+++ utils/findByKey.js
@@ -1,15 +1,18 @@
 const axios = require('axios').default;
 
 const convertHTML = require('./convertHTML');
+const loginToCtu = require('./loginToCtu');
 
 const findByKey = async (payload) => {
 	try {
+		const cookie = await loginToCtu();
+
 		const res = await axios.postForm(
 			'https://qldt.ctu.edu.vn/htql/dkmh/student/index.php?action=dmuc_mhoc_hky',
 			payload,
 			{
 				headers: {
-					cookie: `ZDEDebuggerPresent=php,phtml,php3; PHPSESSID=${process.env.PHPSESSID}`,
+					cookie,
 				},
 			}
 		);
--- utils/loginToCtu.js
+++ utils/loginToCtu.js
@@ -1,15 +1,52 @@
 const axios = require('axios').default;
 
-const loginToCtu = async () => {
+const getCookie = async (txtDinhDanh, txtMatKhau) => {
 	try {
-		const txtDinhDanh = process.env.CODE;
-		const txtMatKhau = process.env.PASSWORD;
 		const res = await axios.postForm('https://qldt.ctu.edu.vn/htql/sinhvien/dang_nhap.php', {
 			txtDinhDanh,
 			txtMatKhau,
 		});
 
 		return res.headers['set-cookie'];
+	} catch (error) {
+		console.log(error);
+	}
+};
+
+const loginToCourses = async (txtDinhDanh, cookie) => {
+	try {
+		await axios.postForm(
+			'https://qldt.ctu.edu.vn/htql/dkmh/student/dang_nhap.php',
+			{
+				txtDinhDanh,
+				txtMatKhau: 'p',
+			},
+			{
+				headers: {
+					cookie,
+				},
+			}
+		);
+	} catch (error) {
+		console.log(error);
+	}
+};
+
+let cookieTemp;
+
+const loginToCtu = async () => {
+	try {
+		if (cookieTemp) return cookieTemp;
+
+		const txtDinhDanh = process.env.CODE;
+		const txtMatKhau = process.env.PASSWORD;
+
+		const cookie = await getCookie(txtDinhDanh, txtMatKhau);
+		await loginToCourses(txtDinhDanh, cookie);
+
+		cookieTemp = cookie;
+
+		return cookie;
 	} catch (error) {
 		console.log(error);
 	}
